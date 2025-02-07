f82e4721ebde98243b405cee8102ee9599fd7dd7
kye1229 <yaeeun0607@gmail.com>
2025-02-07 18:38:56 +0900

    Add files via upload

--- portfolio_ASUS/script/index.js
+++ portfolio_ASUS/script/index.js
@@ -1,9 +1,8 @@
-const bnrSlide = new Swiper('main .bnr_wrap',{
-    autoplay:{
+const bnrSlide = new Swiper('.bnr_wrap',{
+    /* autoplay:{
         delay:2000,
         disableOnInteraction:false,
     },
     loop:true,
-    speed:1000,
-    mousewheel:true,
+    speed:1000, */
 });
