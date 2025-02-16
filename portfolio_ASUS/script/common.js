b550481702efdf31ce771adf514865e432a6f299
yaeeun <yaeeun0607@gmail.com>
2025-02-17 00:32:53 +0900

    Add files via upload

---
+++ portfolio_ASUS/script/common.js
@@ -0,0 +1,6 @@
+const htop = document.querySelector('.h_top');
+const closeBtn = document.querySelector('.h_top #close_btn');
+closeBtn.addEventListener('click',()=>{
+    htop.style.display = 'none';
+});
+
