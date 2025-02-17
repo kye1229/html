1977d1d0183bc80b531dcdea1a16f83e8d1d488a
NEO-KIM369369 <bbkjh1227575@gmail.com>
2025-02-17 15:28:38 +0900

    asus

--- portfolio_ASUS/script/common.js
+++ portfolio_ASUS/script/common.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const htop = document.querySelector('.h_top');
 const closeBtn = document.querySelector('.h_top #close_btn');
 closeBtn.addEventListener('click',()=>{
@@ -24,11 +23,3 @@ searchClose.addEventListener('click',()=>{
     searchPopup.style.display='none';
 })
 console.log(searchPopup);
-=======
-const htop = document.querySelector('.h_top');
-const closeBtn = document.querySelector('.h_top #close_btn');
-closeBtn.addEventListener('click',()=>{
-    htop.style.display = 'none';
-});
-
->>>>>>> b550481702efdf31ce771adf514865e432a6f299
