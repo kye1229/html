9a31f6809913bdff6baf97b2fa247390516c9687
yaeeun <yaeeun0607@gmail.com>
2025-03-10 18:13:49 +0900

    asus

--- project_ASUS/script/common.js
+++ project_ASUS/script/common.js
@@ -4,15 +4,56 @@ closeBtn.addEventListener('click',()=>{
     htop.style.display = 'none';
 });
 
-const tabMenu = document.querySelectorAll('header nav ul li')
-const subTab = document.querySelector('header .sub_tab');
-tabMenu[1].addEventListener('mouseover',()=>{
-    subTab.style.display = 'block';
-})
+const subTab = document.querySelector('.header_left .sub_tab');
+subTab.parentElement.addEventListener('mouseover',()=>{
+    subTab.style.display='block';
+});
 subTab.addEventListener('mouseout',()=>{
-    subTab.style.display = 'none';
-})
+    subTab.style.display='none';
+});
+subTab.addEventListener('mouseout',()=>{
+    subTab.style.display='none';
+});
 
+const moreTab = document.querySelector('.header_right .more_tab');
+const moreTab2 = document.querySelector('.header_right .more_tab2');
+const subT = document.querySelector('.header_right .sub_tab_t');
+const subM = document.querySelector('.header_right .sub_tab_m');
+const subMh2 = document.querySelectorAll('.header_right .sub_tab_m .tab_title');
+const subMmenu = document.querySelectorAll('.header_right .sub_tab_m .sub');
+console.log(subMh2, subMmenu);
+moreTab.addEventListener('click',()=>{
+    subT.classList.toggle('active');
+});
+
+moreTab2.addEventListener('click',()=>{
+    subM.classList.toggle('active');
+});
+
+function removeMsub() {
+
+}
+subMh2[0].addEventListener('click',()=>{
+    subMmenu[0].classList.toggle('active');
+})
+subMh2[1].addEventListener('click',()=>{
+    subMmenu[1].classList.toggle('active');
+})
+subMh2[2].addEventListener('click',()=>{
+    subMmenu[2].classList.toggle('active');
+})
+subMh2[3].addEventListener('click',()=>{
+    subMmenu[3].classList.toggle('active');
+})
+subMh2[4].addEventListener('click',()=>{
+    subMmenu[4].classList.toggle('active');
+})
+subMh2[5].addEventListener('click',()=>{
+    subMmenu[5].classList.toggle('active');
+})
+subMh2[6].addEventListener('click',()=>{
+    subMmenu[6].classList.toggle('active');
+})
 const searchPopup = document.querySelector('header .search_popup');
 const searchIcon = document.querySelector('.header_right .search');
 const searchClose = document.querySelector('.search_popup #close_btn');
@@ -22,4 +63,3 @@ searchIcon.addEventListener('click',()=>{
 searchClose.addEventListener('click',()=>{
     searchPopup.style.display='none';
 })
-console.log(searchPopup);
