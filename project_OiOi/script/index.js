9d626d877600dafeee8ab1c4e676faa873396da0
yaeeun <yaeeun0607@gmail.com>
2025-03-13 01:55:32 +0900

    oioi

--- project_OiOi/script/index.js
+++ project_OiOi/script/index.js
@@ -1,7 +1,13 @@
+const hTop = document.querySelector('.h_top');
+const topClose = document.querySelector('.h_top #close_btn');
+topClose.addEventListener('click',()=>{
+    hTop.style.display='none';
+});
+
 const bnrSwiper = new Swiper ('.bnr_swiper',{
     /* autoplay:{delay:3000,}, */
     scrollbar:{
         el:'.swiper-scrollbar',
-        hide:true,
+        /* hide:true, */
     },
 })
